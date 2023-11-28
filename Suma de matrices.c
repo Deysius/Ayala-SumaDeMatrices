8e805c6c221378c90414740f678443fd595020ce
Josue Ayala <josue.ayala.flores@udla.edu.ec>
2023-11-28 15:09:39 -0500

    Incluyo mis librerias, y pido valores para las filas y columnas

---
+++ Suma de matrices.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(){
+int filas, columnas;
+srand (time(NULL));
+    printf("Ingrese el número de filas de la matriz: ");
+    scanf("%d", &filas);
+    printf("Ingrese el número de columnas de la matriz: ");
+    scanf("%d", &columnas);
+return 0;
+}
